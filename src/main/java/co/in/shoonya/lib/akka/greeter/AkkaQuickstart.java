2727f9b76faec308e8c50fba16b7e80b4933f465
ketan khairnar <ketan.khairnar@gmail.com>
2020-08-17 21:00:15 +0530

    starting with latest akka version

---
+++ src/main/java/co/in/shoonya/lib/akka/greeter/AkkaQuickstart.java
@@ -0,0 +1,35 @@
+package co.in.shoonya.lib.akka.greeter;
+
+import akka.actor.typed.ActorSystem;
+
+import java.io.IOException;
+
+/**
+ * The example consists of three actors:
+ *
+ * Greeter: Receives commands to Greet someone and responds with a Greeted to confirm the greeting has taken place
+ *
+ * GreeterBot: receives the reply from the Greeter and sends a number of additional greeting messages
+ *      and collect the replies until a given max number of messages have been reached.
+ *
+ * GreeterMain: The guardian actor that bootstraps everything
+ */
+public class AkkaQuickstart {
+    public static void main(String[] args) {
+        //#actor-system
+        final ActorSystem<GreeterMain.SayHello> greeterMain = ActorSystem.create(GreeterMain.create(), "helloakka");
+        //#actor-system
+
+        //#main-send-messages
+        greeterMain.tell(new GreeterMain.SayHello("Charles"));
+        //#main-send-messages
+
+        try {
+            System.out.println(">>> Press ENTER to exit <<<");
+            System.in.read();
+        } catch (IOException ignored) {
+        } finally {
+            greeterMain.terminate();
+        }
+    }
+}
